1046f41e1d8be2c93ad4aef2e88027938f51bc28
Miodek <kedoim420@gmail.com>
2022-02-21 12:01:15 +0100

    ML models init

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+#ML models used in house pricing project
+
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
+from sklearn.linear_model import Ridge
+from sklearn.svm import SVR
+from sklearn.ensemble import GradientBoostingRegressor
+
 r_forest = RandomForestRegressor(n_estimators=600, random_state=42)
 xgb = XGBRegressor()
-from sklearn.linear_model import Ridge
-ridge = Ridge()
+ridge = Ridge(alpha = 10,tol = 1e-15)
+svr = SVR(kernel='rbf',C = 20000,epsilon = 0.001)
+gbr = GradientBoostingRegressor(n_estimators=2000,learning_rate=0.01,max_depth=5)
